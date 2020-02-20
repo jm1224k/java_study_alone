62d837a312472050885d1fd4ab9af89aeba23829
NeverDie2020 <wiseidea01@gmail.com>
2020-02-20 23:33:08 +0900

    jeong_mee_did

---
+++ constant/ConstantDemo.java
@@ -0,0 +1,20 @@
+package org.java_home.constant;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		//int a = 2.2;
+		//float b = 2.2;
+		double c = 2.2;
+		float d = 2.2F;
+		
+		//int e = 2147483648;
+		//long f = 2147483648;
+		long g = 2147483648L;
+		
+		//this time, not error operated because java is allow byte and short type to use int type!
+		byte a = 100;
+		short b = 200;
+	}
+
+}
