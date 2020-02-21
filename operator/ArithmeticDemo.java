07e0944679537ea19e2b22f631d5468685ddd3ef
NeverDie2020 <wiseidea01@gmail.com>
2020-02-21 23:18:33 +0900

    jeongmee did

---
+++ operator/ArithmeticDemo.java
@@ -0,0 +1,23 @@
+package org.java_home.operator;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		int result = 1+2;
+		System.out.println(result);
+		
+		result = result - 1;
+		System.out.println(result);
+		
+		result = result * 2;
+		System.out.println(result);
+		
+		result = result / 2;
+		System.out.println(result);
+		
+		result = result + 8;
+		result = result % 7;
+		System.out.println(result);
+	}
+
+}
